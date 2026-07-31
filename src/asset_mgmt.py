b372c2b2cb13e5a1153b4e13e0adc7df76ca251d
esnupi-scary <bunkey1122@gmail.com>
2026-07-31 18:54:54 +0300

    Functionality to recursively copy the contents from a folder to another + a few files to check behavior

---
+++ src/asset_mgmt.py
@@ -0,0 +1,26 @@
+import os, shutil
+
+def copy_contents(source: str, destination: str):
+    print(os.path.exists(source))
+    if not os.path.exists(source):
+        raise Exception("source directory does not exist!")
+    # delete destination
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    copy_recurse(source, destination)
+    pass
+
+def copy_recurse(source:str, destination:str):
+    print(f"source: {source}")
+    print(f"destination: {destination}")
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    for content in os.listdir(source):
+        print(content)
+        if os.path.isdir(f"{source}/{content}"):
+            copy_recurse(f"{source}/{content}", f"{destination}/{content}")
+        else:
+            shutil.copy(f"{source}/{content}", destination)
+    pass
+
+copy_contents("static", "public")
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextType, TextNode
+from asset_mgmt import copy_contents
 
 def main():
     text = TextNode("hello world", TextType.ITALIC)
     print(text)
+    copy_contents("static","public")
 
 main()
